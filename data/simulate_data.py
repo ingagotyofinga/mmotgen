46adad91e1434a91ecfc0c483d2b86b1ba4640a3
ingagotyofinga <ingagirshfeld@gmail.com>
2024-04-26 11:55:37 -0700

    DataSimulator completed for simulating multi-variate data

--- data/simulate_data.py
+++ data/simulate_data.py
@@ -33,27 +33,46 @@ class DataSimulator:
         input_data = torch.cat([source_dists.unsqueeze(1), target_dists.unsqueeze(1)], dim=1)
         return input_data
 
-    def generate_random_data(self):
-        if self.seed is not None:
-            torch.manual_seed(self.seed)
-        source_dists = torch.rand(self.num_distributions, self.num_bins, self.num_dimensions)
-        return source_dists
+    def generate_source_data(self, means, std_devs):
+        dists = []
+        for mean, std_dev in zip(means, std_devs):
+            points = []
+            for i,j in zip(mean,std_dev):
+                dim_points = torch.normal(i,j, size=(1,self.num_bins))
+                points.append(dim_points)
+            points = torch.cat(points,0)
+            points = points.transpose(0,1)
+            dists.append(points)
 
-    def generate_target_from_source(self, source_dists):
+        return torch.stack(dists)
+
+    def generate_target_data(self, source_dists):
         # Generate an affine map
-        affine_map = torch.randn(self.num_dimensions, self.num_dimensions)
-        affine_translation = torch.randn(self.num_dimensions)
+        affine_map = torch.randn(self.num_bins, self.num_bins)
+        affine_translation = torch.randn(self.num_bins,self.num_dimensions)
 
         # Generate a random map
-        random_map = torch.randn(self.num_dimensions, self.num_dimensions)
-        random_translation = torch.randn(self.num_dimensions)
+        random_map = torch.randn(self.num_bins, self.num_bins)
+        random_translation = torch.randn(self.num_bins, self.num_dimensions)
 
         # Compose the affine map and the random map
         composed_map = torch.matmul(random_map, affine_map)
         composed_translation = torch.matmul(random_map, affine_translation) + random_translation
 
+        # Standardize composed_map
+        mean_map = composed_map.mean(dim=0)
+        std_map = composed_map.std(dim=0)
+        standardized_composed_map = (composed_map - mean_map) / std_map
+
+        # Standardize composed_translation
+        mean_translation = composed_translation.mean(dim=0)
+        std_translation = composed_translation.std(dim=0)
+        standardized_composed_translation = (composed_translation - mean_translation) / std_translation
+
         # Apply the composed map to the source to generate the target
-        target_dists = torch.matmul(source_dists, composed_map.t()) + composed_translation
+        target_dists=torch.matmul(composed_map, source_dists) + composed_translation
+
+
         return target_dists
 
     def generate_mu0_distributions(self, source_dists):
@@ -82,18 +101,28 @@ class DataSimulator:
 
 
 # Example usage
-num_distributions = 100
+num_distributions = 10
 num_samples = 100
 num_dimensions = 3
-seed = 42
-simulator = DataSimulator(num_distributions, num_samples, num_dimensions, seed)
-source_dists = simulator.generate_random_data()
-target_dists = simulator.generate_target_from_source(source_dists)
+
+# Means and standard deviations for each cluster in n-dimensional space
+# TODO: build these into the function for simulating data
+means = np.random.randint(low=1,high=500, size=(num_distributions, num_dimensions))
+std_devs = np.random.randint(low=1,high=50, size=(num_distributions, num_dimensions))
+
+simulator = DataSimulator(num_distributions, num_samples, num_dimensions)
+source_dists = simulator.generate_source_data(means,std_devs)
+# TODO: scaling of the target data is off now
+target_dists = simulator.generate_target_data(source_dists)
 
 # Visualize the generated data
+# TODO: visualization script for the data
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(source_dists[:, :, 0], source_dists[:, :, 1], source_dists[:, :, 2], label='Source')
-ax.scatter(target_dists[:, :, 0], target_dists[:, :, 1], target_dists[:, :, 2], label='Target')
-ax.legend()
+for dist in range(len(source_dists)):
+    # ax.scatter(source_dists[dist, :, 0], source_dists[dist, :, 1], source_dists[dist, :, 2], label='Source')
+    ax.scatter(target_dists[dist, :, 0], target_dists[dist, :, 1], target_dists[dist, :, 2], label='Target')
+# ax.legend()
+plt.title("Sampled Target Distributions")
+plt.grid(True)
 plt.show()
